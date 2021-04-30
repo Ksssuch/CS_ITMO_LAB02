7f02533ca51915939f98483fefe29305a844446e
Ksssuch <ksssuch@mail.ru>
2021-05-01 00:46:12 +0300

    Lab02

---
+++ StructType/Struct.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructType
+{
+    public enum AccountType { Checking, Deposit }
+    public struct BankAccount
+    {
+        public long accNo;
+        public decimal accBal;
+        public AccountType accType;
+    }
+    class Struct
+    {
+        static void Main(string[] args)
+        {
+            BankAccount goldAccount;
+            goldAccount.accType = AccountType.Checking;
+            goldAccount.accBal = (decimal)3200.00;
+            Console.WriteLine("Enter account number: ");
+            string Temp = Console.ReadLine();
+
+            goldAccount.accNo = long.Parse (Temp);
+
+
+
+
+            printme(goldAccount);
+        }
+        static void printme(BankAccount account)
+        {
+            Console.WriteLine("Customers account number: {0}, Balance: {1}, Type: {2}",account.accNo,account.accBal,account.accType);
+
+        }
+    }
+}
